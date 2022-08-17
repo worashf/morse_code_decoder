ffe68751bb9e43472fa17738b6cef09d878d7581
worashf <uppert83@gmail.com>
2022-08-18 02:52:54 +0300

    Add morse solution

--- morse_code.rb
+++ morse_code.rb
@@ -1,62 +1,52 @@
-def decode_morse(morseCode)
-    letters = []
-    words = []
-    word = ""
-    sentence = morseCode.split("   ")
-  
-    sentence.each_with_index do |x,i|
-      letters[i] = x.split(" ")
-    end
-  
-    
-    0.upto(letters.length-1) do |i|
-      word = ""
-      0.upto(letters[i].length-1) do |x|
-        case letters[i][x]
-          when "...---..." ; word += "SOS"
-          when "-.-.--" ; word += "!"
-          when ".-.-.-" ; word += "."
-          when ".-" ; word += "A"
-          when "-..." ; word += "B"
-          when "-.-." ; word += "C"
-          when "-.." ; word += "D"
-          when "." ; word += "E"
-          when "..-." ; word += "F"
-          when "--." ; word += "G"
-          when "...." ; word += "H"
-          when ".." ; word += "I"
-          when ".---" ; word += "J"
-          when "-.-" ; word += "K"
-          when ".-.." ; word += "L"
-          when "--" ; word += "M"
-          when "-." ; word += "N"
-          when "---" ; word += "O"
-          when ".--." ; word += "P"
-          when "--.-" ; word += "Q"
-          when ".-." ; word += "R"
-          when "..." ; word += "S"
-          when "-" ; word += "T"
-          when "..-" ; word += "U"
-          when "...-" ; word += "V"
-          when ".--" ; word += "W"
-          when "-..-" ; word += "X"
-          when "-.--" ; word += "Y"
-          when "--.." ; word += "Z"
-          when ".----" ; word += "1"
-          when "..---" ; word += "2"
-          when "...--" ; word += "3"
-          when "....-" ; word += "4"
-          when "....." ; word += "5"
-          when "-...." ; word += "6"
-          when "--..." ; word += "7"
-          when "---.." ; word += "8"
-          when "----." ; word += "9"
-          when "-----" ; word += "0"
-        end
-      end
-      words << word if word != ""
-    end
-    words.join(" ")
+# Create dictionary of caracters
+@caracters = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+# decode each letter
+def letter_decoder(letter)
+  @caracters.each do |key, value|
+    return value if key == letter
   end
-  
-  puts decode_morse(' .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+end
+
+# decode each word
+def word_decoder(word)
+  @current_word = word.split.map { |letter| letter_decoder(letter) }.join
+  "#{@current_word} "
+end
+
+# decode each sentence
+def sentence_decoder(sentence)
+  @current_sentence = sentence.split('   ').map { |word| word_decoder(word) }.join
+  print "#{@current_sentence.downcase} \n"
+end
+
+sentence_decoder('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+sentence_decoder('.-  .-..  .  -..-  .-  -.  -..  .  .-.     ..  ...     ...  ..  -.-.  -.-')
+sentence_decoder('.--  ---  .-.  .-  ...  ....     ..  ...     .-  .-..  .--  .-  -.--  ...     .-..  .-  -  .')
